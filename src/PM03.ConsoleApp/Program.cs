ba80c9f2b2aaec757d046d7adde813946f9b11a8
DavidJan <DavidJan@example.com>
2023-04-18 15:20:48 +0300

    added main projects, for console app, business logic, and tests

---
+++ src/PM03.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PM03.ConsoleApp
+{
+    /// <summary>
+    /// Класс точки входа в программа
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Метод точки входа в программу
+        /// </summary>
+        /// <param name="args">Агрументы запуска консольного приложения</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
