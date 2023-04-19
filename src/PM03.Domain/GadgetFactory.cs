8e9e4fa00de936167453fa93d7732e32b3917338
Davidos533 <medvedkov29@gmail.com>
2023-04-19 08:54:20 +0300

    created new proj for models, added domain models classes Smartphone and GadgetFactory

---
+++ src/PM03.Domain/GadgetFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace PM03.Domain
+{
+    /// <summary>
+    /// Фабрика где хранятся смартфоны (телефоны)
+    /// </summary>
+    public class GadgetFactory
+    {
+        /// <summary>
+        /// Смартфоны
+        /// </summary>
+        public Smartphone[] Smartphones { get; set; }
+    }
+}
---
+++ src/PM03.Domain/Smartphone.cs
@@ -0,0 +1,23 @@
+﻿namespace PM03.Domain
+{
+    /// <summary>
+    /// Смартфон (телефон)
+    /// </summary>
+    public class Smartphone
+    {
+        /// <summary>
+        /// Модель
+        /// </summary>
+        public string Model { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Диагональ
+        /// </summary>
+        public double Digonal { get; set; }
+    }
+}
